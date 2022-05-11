07f1d4ec165f76b77bd9a73e55a83c273f869394
Lukasz Janyst <lukasz@jany.st>
2022-05-11 15:28:04 +0200

    peroxide-cfg: Adjust to the new credentials API
    Issue #13

--- cmd/peroxide-cfg/accounts.go
+++ cmd/peroxide-cfg/accounts.go
@@ -49,19 +49,14 @@ func askPass(prompt string) ([]byte, error) {
 }
 
 func listAccounts(b *bridge.Bridge) {
-	spacing := "%3d: %-20s %-20s %-20s %-15s %-15s "
+	spacing := "%3d: %-20s %-20s %-20s %-15s "
 	for idx, user := range b.Users.GetUsers() {
 		connected := "disconnected"
 		if user.IsConnected() {
 			connected = "connected"
 		}
 
-		mode := "split"
-		if user.IsCombinedAddressMode() {
-			mode = "combined"
-		}
-
-		fmt.Printf(spacing, idx, user.Username(), user.GetBridgePassword(), user.GetPrimaryAddress(), connected, mode)
+		fmt.Printf(spacing, idx, user.Username(), user.GetBridgePassword(), user.GetPrimaryAddress(), connected)
 
 		for _, address := range user.GetAddresses() {
 			fmt.Printf("%-20s", address)
